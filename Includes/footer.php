b6e375dcfe982f83fed7873992406e36b6f52586
Alvin Simaibang <alvinsimaibang@gmail.com>
2025-11-04 10:51:12 +0700

    oke

--- Includes/footer.php
+++
@@ -1,8 +0,0 @@
-</div> 
-<footer class="text-center py-3 mt-4 bg-dark text-light">
-    <small>&copy; <?= date('Y'); ?> Personal Collection Management System</small>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/main.js"></script>
-</body>
-</html>
